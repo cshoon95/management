ef3c0656ede7a5b44fd4572cb5c1e8abeee5d7d8
Soohoon <cshoon950@naver.com>
2021-05-19 22:38:52 +0900

    proxy setting

--- client/src/App.js
+++ client/src/App.js
@@ -19,34 +19,26 @@ const styles = theme => ({
     minWidth: 1080
   }
 })
-const customers = [
-  {
-    'id':1,
-    'image':'https://palceimg.com/64/64/1',
-    'name':'조수훈',
-    'birthday': '950921',
-    'gender' : '남자',
-    'job' : '풀스택 개발자'
-  },
-  {
-    'id':2,
-    'image':'https://palceimg.com/64/64/2',
-    'name':'홍길동',
-    'birthday': '001231',
-    'gender' : '남자',
-    'job' : '백엔드 개발자'
-  },
-  {
-    'id':3,
-    'image':'https://palceimg.com/64/64/3',
-    'name':'이순신',
-    'birthday': '210519',
-    'gender' : '남자',
-    'job' : '프론트 개발자'
-  }
-]
 
 class App extends Component {
+  
+  state = {
+    customers: ""
+  }
+
+  // 데이터를 받아오는 작업
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  } 
+  
   render() {
     const { classes } = this.props;
     return (
@@ -64,7 +56,7 @@ class App extends Component {
           </TableHead>
           <TableBody>
           {
-            customers.map(c => {
+            this.state.customers ? this.state.customers.map(c => {
               return (
                 <Customer
                 key={c.id}
@@ -76,7 +68,7 @@ class App extends Component {
                 job={c.job}
                 />
                 )
-            })
+            }) : ""
           }
             </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id':1,
+            'image':'https://palceimg.com/64/64/1',
+            'name':'조수훈',
+            'birthday': '950921',
+            'gender' : '남자',
+            'job' : '풀스택 개발자'
+        },
+        {
+            'id':2,
+            'image':'https://palceimg.com/64/64/2',
+            'name':'홍길동',
+            'birthday': '001231',
+            'gender' : '남자',
+            'job' : '백엔드 개발자'
+        },
+        {
+            'id':3,
+            'image':'https://palceimg.com/64/64/3',
+            'name':'이순신',
+            'birthday': '210519',
+            'gender' : '남자',
+            'job' : '프론트 개발자'
+        }
+    ]);
 })
 
 app.listen(port, () => console.log('Listening on port ${port}'));
